6c2fa8c5a43be6561ee075f03993a8f0c2ce9677
Mario Avila <mavila0045@gmail.com>
2024-01-23 07:52:02 -0800

    Updated Visual and Script

--- restaurant_tables.sql
+++ restaurant_tables.sql
@@ -17,30 +17,23 @@ CREATE TABLE orders (
     appetizers VARCHAR(30),
     entrees VARCHAR(30),
     desserts VARCHAR(30),
-    total VARCHAR(30)
+    total VARCHAR(30),
+    FOREIGN KEY (customer_id) REFERENCES customer_statistics(customer_id)
 );
 
 CREATE TABLE customer_demographics (
-    customer_id INT PRIMARY KEY,
+    customer_demographic_id VARCHAR(40) PRIMARY KEY,
     address VARCHAR(95),
     city VARCHAR(35),
     credit_card_expires CHAR(5),
     credit_card_number VARCHAR(19),
     credit_card_provider VARCHAR(50),
     credit_card_security_code INT,
-    customer_second_id VARCHAR(40),
     email VARCHAR(62),
     name VARCHAR(50),
     phone_number VARCHAR(30),
     state VARCHAR(50),
-    zip_code INT
+    zip_code INT,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customer_statistics(customer_id)
 );
-
-ALTER TABLE customer_statistics
-ADD FOREIGN KEY (customer_id) REFERENCES customer_demographics(customer_id);
-
-ALTER TABLE orders
-ADD FOREIGN KEY (customer_id) REFERENCES customer_statistics(customer_id);
-
-ALTER TABLE customer_demographics
-ADD FOREIGN KEY (customer_id) REFERENCES customer_statistics(customer_id);
