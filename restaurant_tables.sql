dd7cc8d4da5201db8d17efe0070c751a3f89c674
Mario Avila <mavila0045@gmail.com>
2024-01-23 12:10:16 -0800

    Updated to match clean data

--- restaurant_tables.sql
+++ restaurant_tables.sql
@@ -23,17 +23,17 @@ CREATE TABLE orders (
 
 CREATE TABLE customer_demographics (
     customer_demographic_id VARCHAR(40) PRIMARY KEY,
+    customer_id INT,
+    name VARCHAR(50),
+    email VARCHAR(62),
+    phone_number VARCHAR(30),
     address VARCHAR(95),
     city VARCHAR(35),
-    credit_card_expires CHAR(5),
-    credit_card_number VARCHAR(19),
-    credit_card_provider VARCHAR(50),
-    credit_card_security_code INT,
-    email VARCHAR(62),
-    name VARCHAR(50),
-    phone_number VARCHAR(30),
     state VARCHAR(50),
     zip_code INT,
-    customer_id INT,
+    credit_card_number VARCHAR(19),
+    credit_card_expires CHAR(5),
+    credit_card_security_code INT,
+    credit_card_provider VARCHAR(50),
     FOREIGN KEY (customer_id) REFERENCES customer_statistics(customer_id)
 );
